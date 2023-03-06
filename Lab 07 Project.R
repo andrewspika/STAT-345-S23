c4d769572f4f15872ef0e3ede400930e013feda3
andrewspika <andrewspika@gmail.com>
2023-03-06 12:51:18 -0600

    Lab 07 submission information

--- Lab 07 Project.R
+++ Lab 07 Project.R
@@ -1,3 +1,8 @@
 print("Lab 07 Git file with updates")
-print("Modification number 1")
-# Retrying git commit to the repository
+
+# Motification number 3 is removing two lines of code and writing some code
+print("This is my lab 07")
+5 + 5
+14 + 6
+a <- "Andrew Spika"
+print(a)
